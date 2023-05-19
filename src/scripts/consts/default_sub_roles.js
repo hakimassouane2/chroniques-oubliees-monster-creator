c8ad93f8beefe07648c00d2a5ccc0dd3e0e56668
hakimassouane2 <hakimassouane@hotmail.com>
2023-05-19 15:30:18 +0900

    Added sub roles const

---
+++ src/scripts/consts/default_sub_roles.js
@@ -0,0 +1,61 @@
+/**
+ * Default monster sub roles.
+ */
+const DEFAULT_SUB_ROLES = {
+  hexer: {
+    id: "hexer",
+  },
+  shaper: {
+    id: "shaper",
+  },
+  tactician: {
+    id: "tactician",
+  },
+  bulwark: {
+    id: "bulwark",
+  },
+  guardian: {
+    id: "guardian",
+  },
+  sentinel: {
+    id: "sentinel",
+  },
+  assassin: {
+    id: "assassin",
+  },
+  exploiter: {
+    id: "exploiter",
+  },
+  sneak: {
+    id: "sneak",
+  },
+  spotter: {
+    id: "spotter",
+  },
+  evader: {
+    id: "evader",
+  },
+  traveller: {
+    id: "traveller",
+  },
+  butcher: {
+    id: "butcher",
+  },
+  deadeye: {
+    id: "deadeye",
+  },
+  havoc: {
+    id: "havoc",
+  },
+  booster: {
+    id: "booster",
+  },
+  leader: {
+    id: "leader",
+  },
+  mender: {
+    id: "mender",
+  },
+};
+
+export default DEFAULT_SUB_ROLES;
